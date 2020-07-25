993ffdcf484a71d0c47291b2eb61bcb88a656870
oybek <djurabekov123@gmail.com>
2020-07-25 17:54:59 +0500

    fix

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
     if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
         $('.player').hide()
     }
-    let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+//     let vh = window.innerHeight * 0.01;
+// // Then we set the value in the --vh custom property to the root of the document
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
   $('.eye').click(function(){
     let input = $(this).siblings('input')
     input.attr('type') === 'password'? input.attr('type', 'text') : input.attr('type', 'password')
