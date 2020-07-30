e04bb4977f8bbf304eaf6c16790ade4edfcc3f4f
oybek <djurabekov123@gmail.com>
2020-07-30 12:49:11 +0500

    desktop version

--- frontend/js/recorder.js
+++ frontend/js/recorder.js
@@ -1,24 +1,25 @@
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
+var minutesLabel = $(".player:visible .minutes");
+var secondsLabel = $(".player:visible .seconds");
 var totalSeconds = 0;
 var timer;
+console.log(minutesLabel)
 $(".recorder").bind('mousedown  touchstart', function(){
     $(this).parent('.thumbs').addClass('active').removeClass('unactive')
     $('.delay').show();
     timer = setInterval(setTime, 1000);
 }).bind('mouseup touchend', function(){
-    $(this).parent('.thumbs').removeClass('active').addClass('stop')
-    $(this).parent('.thumbs').find('.record').toggle()
-    $(this).parent('.thumbs').find('.playingbutton').toggle()
+    $(this).parents('.player').find('.thumbs').removeClass('active').addClass('stop')
+    $(this).parents('.player').find('.thumbs').find('.record').toggle()
+    $(this).parents('.player').find('.thumbs').find('.playingbutton').toggle()
     console.log('Stop Recording...');
     clearInterval(timer)
 });
 
 function refreshRecord(el){
-    $(el).parent('.thumbs').removeClass('stop').addClass('unactive')
-    $(el).parent('.thumbs').find('.record').toggle()
-    $(el).parent('.thumbs').find('.playingbutton').toggle()
-    $(el).parent('.thumbs').find('.delay').toggle()
+    $(el).parents('.player').find('.thumbs').removeClass('stop').addClass('unactive')
+    $(el).parents('.player').find('.thumbs').find('.record').toggle()
+    $(el).parents('.player').find('.thumbs').find('.playingbutton').toggle()
+    $(el).parents('.player').find('.thumbs').find('.delay').toggle()
     secondsLabel.innerHTML = '00';
     minutesLabel.innerHTML = '00';
     totalSeconds = 0;
@@ -28,8 +29,8 @@ function refreshRecord(el){
 function setTime()
 {
     ++totalSeconds;
-    secondsLabel.innerHTML = pad(totalSeconds%60);
-    minutesLabel.innerHTML = pad(parseInt(totalSeconds/60));
+    secondsLabel.text(pad(totalSeconds%60));
+    minutesLabel.text(pad(parseInt(totalSeconds/60)));
 }
 function pad(val)
 {
